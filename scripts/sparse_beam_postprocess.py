98364ca843a6beb729438eb8b6b06d01ef7a445e
Mike Wilensky <wilensky.michael@gmail.com>
2026-06-25 19:22:57 -0500

    Finish plotting code (untested)

--- scripts/sparse_beam_postprocess.py
+++ scripts/sparse_beam_postprocess.py
@@ -1,8 +1,10 @@
 import argparse
 from itertools import product
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 from embers_passes import SphericalSpline, eval_spline_batch, make_knots_from_grid, eval_spline_samples
 from sparse_beam_fit import prep_spline_params
@@ -79,7 +81,73 @@ def eval_peak_norm(coeffs):
     return val
 
 # Gives a pytree as above but with peak values
-peak_norms = jax.tree.map(eval_peak_norm, outputs) 
+peak_samps = jax.tree.map(eval_peak_norm, outputs)
+peak_means = jax.tree.map(jnp.mean, peak_samps)
+peak_std = jax.tree.map(jnp.std, peak_samps)
+
+fig, ax = plt.subplots(ncols=2)
+pol_markers = {"XX": ".", "YY": "x"}
+jk_half_colors = ["tab:blue", "tab:orange"]
+
+capsize = 4
+for tile, pol, jk_mode, jk_half in product(*product_args):
+    jk_mode_ind = jk_mode == "time"
+    this_ax = ax[jk_mode_ind]
+    this_ax.set_title(f"{jk_mode} jackknife")
+
+    universal_kwargs = {
+        "marker": pol_markers[pol], 
+        "capsize": capsize,
+        "linestyle": "none"
+    }
+    
+    this_ax.errorbar(
+        int(tile), 
+        peak_means[tile][pol]["full_mcmc"], 
+        peak_std[tile][pol]["full_mcmc"],
+        color="black",
+        **universal_kwargs
+    )
+
+    this_ax.errorbar(
+        int(tile),
+        peak_means[tile][pol][jk_mode][jk_half],
+        peak_std[tile][pol][jk_mode][jk_half]
+        color=jk_half_colors[jk_half],
+        **universal_kwargs
+    )
+
+    this_ax.set_xlabel("Tile Number")
+    this_ax.set_ylabel("Value at Zenith")
+
+proxies = []
+for pol in pols:
+    for jk_half in jk_halves:
+        proxy = mlines.Line2D(
+            [], 
+            [], 
+            color=jk_half_colors[jk_half], 
+            marker=pol_markers[pol],
+            label=f"{pol}, jk half {jk_half}",
+            linestyle="none"
+        )
+        proxies.append(proxy)
+proxies.append(mlines.Line2D(
+    [], 
+    [], 
+    color="black", 
+    marker=".", 
+    label="full dataset"
+))
+fig.legend(handles=proxies)
+fig.tight_layout()
+outdir = f"{args.indir}/plots"
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+fig.savefig(f"{outdir}/peak_check.pdf")
+
+
+
 
 
 
